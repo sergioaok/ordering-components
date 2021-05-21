bb0a901bb2b9ff203c5b82bfb008806c1737ba18
Yornel <yornelmarval@gmail.com>
2021-05-21 13:57:33 -0400

    updating

--- example/components/GoogleLoginUI/index.js
+++ example/components/GoogleLoginUI/index.js
@@ -17,7 +17,7 @@ export const GoogleLoginUI = (props) => {
       {beforeComponents.map((BeforeComponent, i) => (
         <BeforeComponent key={i} {...props} />
       ))}
-      {<h2>Google Login</h2>}
+      {<h2>Google Login test</h2>}
       {<div onClick={signIn} id='my-signin2'>Login</div>}
       {afterComponents.map((AfterComponent, i) => (
         <AfterComponent key={i} {...props} />
